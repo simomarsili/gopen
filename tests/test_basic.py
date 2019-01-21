dcb613a12ef2a110bab900a28916f98179aac747
simomarsili <simomarsili@gmail.com>
2019-01-21 16:25:01 +0100

    tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,19 @@
 import os
+import pytest
 import gopen
 STRING = 'qux\nquux\nquuz\n'
+
+
+def _compress(path, compressor):
+    import subprocess
+    import os.path
+    extension = {'gzip': '.gz', 'bzip2': '.bz2'}
+    if compressor in extension:
+        subprocess.check_call('%s %s' % (compressor, path), shell=True)
+    else:
+        raise ValueError('Possible compressors are: %s, %s' % extension.keys())
+    path1 = path + extension[compressor]
+    return path1
 
 
 def _test_dir():
@@ -10,37 +23,30 @@ def _test_dir():
     if basename == 'tests':
         return cwd
     else:
-        tests_dir = os.path.join(cwd, 'tests')
-        if os.path.exists(tests_dir):
-            return tests_dir
+        test_dir = os.path.join(cwd, 'tests')
+        if os.path.exists(test_dir):
+            return test_dir
 
 
-def _check_content(source):
-    file_name = os.path.join(_tests_dir(), source)
-    with gopen.gopen(file_name) as fp:
-        assert fp.read() == STRING
-
-
-def test_basic():
-    _check_content('txt')
-
-
-def test_gzip():
-    _check_content('txt.gz')
-
-
-def test_bzip2():
-    _check_content('txt.gz')
-
-
-def test_file_descriptor():
+@pytest.fixture()
+def tmp_file():
     import tempfile
     import os
     fd, fpath = tempfile.mkstemp(text=True)
     with open(fpath, 'w') as f:
-        print('foo', file=f, end='')
-    with gopen.gopen(fd) as fp:
-        assert fp.read() == 'foo'
+        print(STRING, file=f, end='')
+    yield {'fd': fd, 'file': fpath}
     os.close(fd)
     os.remove(fpath)
 
+
+def test_basic(tmp_file):
+    file_path = tmp_file['file']
+    with gopen.gopen(file_path) as fp:
+        assert fp.read() == STRING
+
+
+def test_file_descriptor(tmp_file):
+    fd = tmp_file['fd']
+    with gopen.gopen(fd) as fp:
+        assert fp.read() == STRING
